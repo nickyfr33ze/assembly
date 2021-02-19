70d3489625b7b8f8eed6d54a578500dce6def979
Nick <43529027+nickyfr33ze@users.noreply.github.com>
2021-02-18 21:44:37 -0700

    Added new palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,24 @@
+//?#include <stdio.h>
+
+/**
+* Check if word is a palindrome
+* @param word
+* @return 0 iff word is palindrome, nonzero otherwise.
+*/
+int palindrome(char *word) {
+   char *a = word;
+   char *b = word;
+   while(*(b + 1) != 0) b++;
+   while (*a == *b && a < b) {
+       a++;
+       b--;
+   }
+   return *a - *b;
+}
+
+int main() {
+   if (palindrome("racecar") == 0) {
+       printf("racecar is a palindrome.\n");
+   }
+   return 0;
+}
