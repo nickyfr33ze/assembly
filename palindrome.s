cbc3a736bafb52053f47ce0ea0e1c72d6e45ed56
Nick <43529027+nickyfr33ze@users.noreply.github.com>
2021-02-27 16:20:01 -0700

    Updated

---
+++ palindrome.s
@@ -0,0 +1,10 @@
+/* 
+Need to write out structure
+
+What you need:
+- A cluster of chars (a string)
+- Pass that into a function that will determine if palindrome
+- Pass 'palindrome' into a main function
+  - Make sure main() returns a true or false
+
+*/ 
