eab67c40ca917e84ed9ce41de35486b0f4750760
Nick <43529027+nickyfr33ze@users.noreply.github.com>
2021-03-01 09:28:47 -0700

    Updated the code from RPi4

--- palindrome.s
+++ palindrome.s
@@ -8,3 +8,24 @@ What you need:
   - Make sure main() returns a true or false
 
 */ 
+        .data
+arry1:  .word   1,2,3,4,5
+str1:   .asciz  "MADAM"
+arry2:  .skip   80      @20 element array
+
+        @ r0 = *word    // M
+        @ r1 = *word    // A
+        @ r2 = *word    // D
+        @ r3 = *word    // A
+        @ r4 = *word    // M
+        @ r5 = word     //"MADAM str
+        @ r6 = word     //"MADAM str
+        @ r8 = i        //pointer 1
+        @ r9 = i        //pointer 2
+
+main:   ldr r5, [r0,r1,r2,r3,r4]        //stores r0 -> r4 into r5
+        ldr r6, r5                      //stores r5 into r6
+
+loop1:  cmp r0, r4
+        bge end1OA
+
