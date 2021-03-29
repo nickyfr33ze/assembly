71a1a5f82aea88f8dfcef8cbe11e07545a2a13dd
Nick <43529027+nickyfr33ze@users.noreply.github.com>
2021-03-29 16:03:21 -0600

    Finsihed product

---
+++ exercise3/solution.s
@@ -0,0 +1,34 @@
+    .text
+    .global main
+
+main:
+    sub sp, sp, #4
+    str lr, [sp, #0]
+
+# Prompt For An Input
+    ldr r0, =prompt
+    bl  printf
+
+#Scanf
+    ldr r0, =format
+    sub sp, sp, #4
+    mov r1, sp
+    bl  scanf
+    ldr r2, [sp, #0]
+    add sp, sp, #4
+
+# Printing The Message
+    mov r1, r2
+    bl  printf
+
+    ldr lr, [sp, #0]
+    add sp, sp, #4
+    mov pc, lr
+
+    .data
+
+format:
+    .asciz "Your Number Is %d \n"
+
+prompt:
+    .asciz "Enter A Number\n"
