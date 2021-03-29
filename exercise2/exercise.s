f5720062d4fec1755b129fcf10ddbfe63cc92696
Nick <43529027+nickyfr33ze@users.noreply.github.com>
2021-03-29 14:44:14 -0600

    update

--- exercise2/exercise.s
+++ exercise2/exercise.s
@@ -8,11 +8,11 @@ ofmt:   .asciz  "the average is %d\n"
 avg:    stmfd   sp!, {lr}
         // your implementation goes here
         
-        mov     r2, =array      // moves the array in to r2
-        mov     r3, #10         // this moves the value 10 into r3
-        div     r3              //divides r2 by r3
-        ldr     r0, r3          // loads our retrun register (r0) with the answer in r3
-        bl      avg             //finishes the program
+        mov     r2, =array      @ moves the array in to r2
+        mov     r3, #10         @ this moves the value 10 into r3
+        div     [r3]            @ divides r2 by r3
+        ldr     r0, r3          @ loads our retrun register (r0) with the answer in r3
+        bl      avg             @ finishes the program
 
         // remember to set r0 to the return result
         ldmfd   sp!, {lr}
